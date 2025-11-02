e903b12a0164bc128c795d8bfc2708ee230fec3b
like115 <258861342@qq.com>
2025-11-02 10:22:41 +0800

    td

---
+++ tkinter/ui.py
@@ -0,0 +1,92 @@
+"""
+本代码由[Tkinter布局助手]生成
+官网:https://www.pytk.net
+QQ交流群:905019785
+在线反馈:https://support.qq.com/product/618914
+"""
+import random
+from tkinter import *
+from tkinter.ttk import *
+class WinGUI(Tk):
+    def __init__(self):
+        super().__init__()
+        self.__win()
+        self.tk_label_mhfot717 = self.__tk_label_mhfot717(self)
+        self.tk_text_mhfotkpx = self.__tk_text_mhfotkpx(self)
+        self.tk_button_mhfotu1b = self.__tk_button_mhfotu1b(self)
+        self.tk_button_mhfotw7b = self.__tk_button_mhfotw7b(self)
+    def __win(self):
+        self.title("Tkinter布局助手")
+        # 设置窗口大小、居中
+        width = 600
+        height = 500
+        screenwidth = self.winfo_screenwidth()
+        screenheight = self.winfo_screenheight()
+        geometry = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.geometry(geometry)
+        
+        self.resizable(width=False, height=False)
+        
+    def scrollbar_autohide(self,vbar, hbar, widget):
+        """自动隐藏滚动条"""
+        def show():
+            if vbar: vbar.lift(widget)
+            if hbar: hbar.lift(widget)
+        def hide():
+            if vbar: vbar.lower(widget)
+            if hbar: hbar.lower(widget)
+        hide()
+        widget.bind("<Enter>", lambda e: show())
+        if vbar: vbar.bind("<Enter>", lambda e: show())
+        if vbar: vbar.bind("<Leave>", lambda e: hide())
+        if hbar: hbar.bind("<Enter>", lambda e: show())
+        if hbar: hbar.bind("<Leave>", lambda e: hide())
+        widget.bind("<Leave>", lambda e: hide())
+    
+    def v_scrollbar(self,vbar, widget, x, y, w, h, pw, ph):
+        widget.configure(yscrollcommand=vbar.set)
+        vbar.config(command=widget.yview)
+        vbar.place(relx=(w + x) / pw, rely=y / ph, relheight=h / ph, anchor='ne')
+    def h_scrollbar(self,hbar, widget, x, y, w, h, pw, ph):
+        widget.configure(xscrollcommand=hbar.set)
+        hbar.config(command=widget.xview)
+        hbar.place(relx=x / pw, rely=(y + h) / ph, relwidth=w / pw, anchor='sw')
+    def create_bar(self,master, widget,is_vbar,is_hbar, x, y, w, h, pw, ph):
+        vbar, hbar = None, None
+        if is_vbar:
+            vbar = Scrollbar(master)
+            self.v_scrollbar(vbar, widget, x, y, w, h, pw, ph)
+        if is_hbar:
+            hbar = Scrollbar(master, orient="horizontal")
+            self.h_scrollbar(hbar, widget, x, y, w, h, pw, ph)
+        self.scrollbar_autohide(vbar, hbar, widget)
+    def __tk_label_mhfot717(self,parent):
+        label = Label(parent,text="说明",anchor="center", )
+        label.place(x=68, y=47, width=182, height=35)
+        return label
+    def __tk_text_mhfotkpx(self,parent):
+        text = Text(parent)
+        text.place(x=70, y=110, width=458, height=239)
+        return text
+    def __tk_button_mhfotu1b(self,parent):
+        btn = Button(parent, text="取消", takefocus=False,)
+        btn.place(x=368, y=388, width=50, height=30)
+        return btn
+    def __tk_button_mhfotw7b(self,parent):
+        btn = Button(parent, text="确定", takefocus=False,)
+        btn.place(x=459, y=385, width=50, height=30)
+        return btn
+class Win(WinGUI):
+    def __init__(self, controller):
+        self.ctl = controller
+        super().__init__()
+        self.__event_bind()
+        self.__style_config()
+        self.ctl.init(self)
+    def __event_bind(self):
+        pass
+    def __style_config(self):
+        pass
+if __name__ == "__main__":
+    win = WinGUI()
+    win.mainloop()
